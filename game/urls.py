3f4de104dea331573a410e3fbbdb4022bf88cd00
Aditya Kumar Gupta <aditya.rakesh.2005@gmail.com>
2026-02-08 13:23:25 +0530

    api: Add move validation and legal moves endpoints.

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('move/', views.make_move, name='make_move'),
+    path('api/move/', views.make_move, name='make_move'),
+    path('api/valid-moves/', views.valid_moves, name='valid_moves'),
+    path('api/new-game/', views.new_game, name='new_game'),
 ]
