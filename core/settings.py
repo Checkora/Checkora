1004b08066ad961e839fae9084d4c8d47cc8e4c7
EDWARD-012 <ravi5258p@gmail.com>
2026-05-03 15:02:50 +0530

    fix: restore premium UI and fix static files configuration

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "game" / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
