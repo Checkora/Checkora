7ad2ce465a68b624d6f3bc9e7e6501af3f47c852
Nikkiraj4 <nikitakumari4614@gmail.com>
2026-07-11 22:10:38 +0530

    test: Add pagination tests for Your Discussions and Bookmarked tabs.

--- game/test_forum.py
+++ game/test_forum.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from game.models import Discussion
+from game.models import Discussion, DiscussionBookmark
 
 User = get_user_model()
 
@@ -61,3 +61,79 @@ class ForumPaginationTests(TestCase):
         
         html = response.content.decode("utf-8")
         self.assertIn("?page=1&sort=oldest", html)
+
+
+class ForumYourDiscussionsPaginationTests(TestCase):
+    """The 'Your Discussions' tab must paginate like the 'All Discussions' tab."""
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser2", password="password")
+        self.client.login(username="testuser2", password="password")
+
+        for i in range(22):
+            Discussion.objects.create(
+                user=self.user,
+                title=f"My Discussion {i}",
+                content=f"Content {i}"
+            )
+        self.forum_url = reverse("forum")
+
+    def test_your_discussions_first_page_is_capped_at_20(self):
+        response = self.client.get(self.forum_url)
+        self.assertEqual(response.status_code, 200)
+
+        your_page_obj = response.context["your_page_obj"]
+        self.assertEqual(your_page_obj.paginator.num_pages, 2)
+        self.assertEqual(len(your_page_obj), 20)
+        self.assertTrue(your_page_obj.has_next())
+
+    def test_your_discussions_second_page(self):
+        response = self.client.get(self.forum_url, {"your_page": 2})
+        self.assertEqual(response.status_code, 200)
+
+        your_page_obj = response.context["your_page_obj"]
+        self.assertEqual(your_page_obj.number, 2)
+        self.assertEqual(len(your_page_obj), 2)
+
+    def test_paging_your_discussions_does_not_affect_main_tab(self):
+        response = self.client.get(self.forum_url, {"your_page": 2, "page": 1})
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context["your_page_obj"].number, 2)
+        self.assertEqual(response.context["page_obj"].number, 1)
+
+
+class ForumBookmarkedDiscussionsPaginationTests(TestCase):
+    """The 'Bookmarked' tab must paginate like the 'All Discussions' tab."""
+
+    def setUp(self):
+        self.owner = User.objects.create_user(username="owner", password="password")
+        self.user = User.objects.create_user(username="bookmarker", password="password")
+        self.client.login(username="bookmarker", password="password")
+
+        for i in range(22):
+            discussion = Discussion.objects.create(
+                user=self.owner,
+                title=f"Bookmarkable Discussion {i}",
+                content=f"Content {i}"
+            )
+            DiscussionBookmark.objects.create(user=self.user, discussion=discussion)
+
+        self.forum_url = reverse("forum")
+
+    def test_bookmarked_first_page_is_capped_at_20(self):
+        response = self.client.get(self.forum_url)
+        self.assertEqual(response.status_code, 200)
+
+        bookmarked_page_obj = response.context["bookmarked_page_obj"]
+        self.assertEqual(bookmarked_page_obj.paginator.num_pages, 2)
+        self.assertEqual(len(bookmarked_page_obj), 20)
+        self.assertTrue(bookmarked_page_obj.has_next())
+
+    def test_bookmarked_second_page(self):
+        response = self.client.get(self.forum_url, {"bookmarked_page": 2})
+        self.assertEqual(response.status_code, 200)
+
+        bookmarked_page_obj = response.context["bookmarked_page_obj"]
+        self.assertEqual(bookmarked_page_obj.number, 2)
+        self.assertEqual(len(bookmarked_page_obj), 2)
